042d318ee5ff1f36756975fad39365d8b4977c5f
Matt Evans <mattevans98@gmail.com>
2020-07-18 20:20:37 -0500

    Initial commit

---
+++ components/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Switch, Text, View} from 'react-native';
+
+export const LangingPage = () => {
+	return (
+		<View>
+			<Text>Welcome to an unnamed finance app!</Text>
+			<Text>Press here to switch to Light or Dark Mode!</Text>
+			<Switch></Switch>
+		</View>
+	)
+}
