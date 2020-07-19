92ae0c81a90c21ecc33ca5194c26a3a115d134c0
Matt Evans <mattevans98@gmail.com>
2020-07-19 02:10:13 -0500

    Added React Navigation theme method

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,6 +1,9 @@
 const palette = {
 	primaryGreen: '#66ff68',
 	primaryDarkGray: '#1e1e1e',
+	secondaryGreen: '#145a0a',
+	borderColor: '#8bff8b',
+	redAlert: '#ff3232',
 	white: '#fff'
 };
 
@@ -10,13 +13,37 @@ const colors = {
 	primaryAccent: palette.primaryGreen
 };
 
-export const themedColors = {
-	light: {
-		...colors
-	},
-	dark: {
-		standardText: palette.white,
-		bgColor: palette.primaryDarkGray,
-		primaryAccent: palette.primaryGreen
+export const lightTheme = {
+	dark: false,
+	colors: {
+		primary: palette.primaryGreen,
+		background: palette.white,
+		card: palette.secondaryGreen,
+		text: palette.primaryDarkGray,
+		border: palette.borderColor,
+		notification: palette.redAlert
 	}
 };
+
+export const darkTheme = {
+	dark: true,
+	colors: {
+		primary: palette.primaryGreen,
+		background: palette.primaryDarkGray,
+		card: palette.secondaryGreen,
+		text: palette.white,
+		border: palette.borderColor,
+		notification: palette.redAlert
+	}
+};
+
+// export const themedColors = {
+// 	light: {
+// 		...colors
+// 	},
+// 	dark: {
+// 		standardText: palette.white,
+// 		bgColor: palette.primaryDarkGray,
+// 		primaryAccent: palette.primaryGreen
+// 	}
+// };
